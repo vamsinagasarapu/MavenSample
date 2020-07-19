6d2974fa56513b238f23624f357d2e2e7822bba2
Vamsi Krishna <vamsi.nagasarapu@gmail.com>
2020-07-19 10:40:02 +0530

    updated exec.java for checking purpose

--- src/main/java/com/vamsi/test/Exec.java
+++ src/main/java/com/vamsi/test/Exec.java
@@ -11,6 +11,7 @@ public class Exec {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Vamsi Krishna\\Programming_support_files\\browser-drivers\\chrome\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://adactinhotelapp.com/index.php");
+		System.out.printil("added for checking purpose");
 	}
 
 }
